159eec9e10d438169d4784cb823d21a18e0dbff5
agoenks29D <agungmasda29@gmail.com>
2022-01-19 19:53:42 +0700

    load app config

---
+++ application/config/app.php
@@ -0,0 +1,13 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * @package Codeigniter
+ * @subpackage App
+ * @category Config
+ * @author Agung Dirgantara <agungmasda29@gmail.com>
+ */
+
+$config['app_name'] = 'Skripsi';
+$config['app_user'] = 'Agung Dirgantara';
+
+/* End of file app.php */
+/* Location : ./application/config/app.php */
--- application/config/autoload.php
+++ application/config/autoload.php
@@ -104,7 +104,7 @@ $autoload['helper'] = array('cookie', 'date', 'directory', 'email', 'html', 'lan
 | config files.  Otherwise, leave it blank.
 |
 */
-$autoload['config'] = array();
+$autoload['config'] = array('app');
 
 /*
 | -------------------------------------------------------------------
