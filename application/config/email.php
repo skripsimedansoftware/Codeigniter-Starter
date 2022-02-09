062dfd883f6a70b8e22e1bf8157afeda4189f843
agoenks29D <agungmasda29@gmail.com>
2022-02-10 05:49:49 +0700

    track config

---
+++ application/config/email.php
@@ -0,0 +1,26 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * @package Codeigniter
+ * @subpackage Email
+ * @category Config
+ * @author Agung Dirgantara <agungmasda29@gmail.com>
+ */
+
+$config['useragent'] = 'Skripsi';
+$config['protocol'] = 'smtp';
+$config['smtp_name'] = 'Skripsi';
+$config['smtp_host'] = '';
+$config['smtp_user'] = '';
+$config['smtp_pass'] = '';
+$config['smtp_port'] = 465;
+$config['smtp_crypto'] = 'ssl';
+$config['wordwrap'] = TRUE;
+$config['validate'] = FALSE;
+$config['priority'] = 3;
+$config['mailtype'] = 'html';
+$config['charset'] = 'utf-8';
+$config['smtp_timeout'] = 10;
+$config['smtp_keepalive'] = FALSE;
+
+/* End of file email.php */
+/* Location : ./application/config/email.php */
